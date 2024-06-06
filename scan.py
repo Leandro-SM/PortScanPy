a55e6911c698578fa1b6f9039e3f6dfcb87d590b
Leandro Medeiros <65362520+Leandro-SM@users.noreply.github.com>
2024-06-06 12:27:50 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,24 +1,27 @@
 import socket
 import ipaddress
+import math
+import errno
+
 
 def gerar_ips(inicio, fim):
-    inicio_ip = ipaddress.ip_address(inicio)
-    fim_ip = ipaddress.ip_address(fim)
-    ip_list = []
+    primeiro_ip = ipaddress.ip_address(inicio)
+    ultimo_ip = ipaddress.ip_address(fim)
+    lista_ips = []
     
-    for net in ipaddress.summarize_address_range(inicio_ip, fim_ip):
+    for net in ipaddress.summarize_address_range(primeiro_ip, ultimo_ip):
         for ip in net:
-            ip_list.append(str(ip))
+            lista_ips.append(str(ip))
     
-    return ip_list
-
-inicio_ip = input("Primeiro IP\n")
-fim_ip = input("Ultimo IP\n")
-ips = gerar_ips(inicio_ip, fim_ip)
+    return lista_ips
+print("Informe os Hosts a serem escaneados")
+primeiro_ip = input("Primeiro IP\n")
+ultimo_ip = input("Ultimo IP\n")
+ips = gerar_ips(primeiro_ip, ultimo_ip)
 
 portas = [22, 80, 443, 8080, 8443]
 for ip in ips:
-    print(f"\nVerificando.. {ip}")
+    print(f"\nVerificando host: {ip}")
     for porta in portas:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1) 
